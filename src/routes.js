b09bf7c6b1bcb5b57fa878a19a151afe6797f63c
Diogo Amaral <diogo-amaral@live.com>
2019-03-16 20:23:14 -0300

    Validations

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,26 @@
 const express = require('express')
+const validate = require('express-validation')
+const handle = require('express-async-handler')
 
 const routes = express.Router()
 
 const authMiddlware = require('./app/middlwares/auth')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.get('/users', controllers.UserController.index)
-routes.post('/users', controllers.UserController.store)
-routes.delete('/users/:id', controllers.UserController.destroy)
-routes.post('/sessions', controllers.SessionController.store)
+// routes.get('/users', controllers.UserController.index)
+routes.post(
+  '/users',
+  validate(validators.User),
+  handle(controllers.UserController.store)
+)
+// routes.delete('/users/:id', controllers.UserController.destroy)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  handle(controllers.SessionController.store)
+)
 
 routes.use(authMiddlware)
 
@@ -17,16 +28,28 @@ routes.use(authMiddlware)
  *  Ads
  **/
 
-routes.get('/ads', controllers.AdController.index)
-routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
-routes.delete('/ads/:id', controllers.AdController.destroy)
+routes.get('/ads', handle(controllers.AdController.index))
+routes.get('/ads/:id', handle(controllers.AdController.show))
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+)
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  handle(controllers.AdController.update)
+)
+routes.delete('/ads/:id', handle(controllers.AdController.destroy))
 
 /**
  * Purchases
  */
 
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  handle(controllers.PurchaseController.store)
+)
 
 module.exports = routes
